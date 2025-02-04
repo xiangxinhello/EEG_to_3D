623b0cc472a7f6ed1b3304b5def3c530754d5875
xiangxinhello <xiangxin199621@gmail.com>
2025-02-04 12:06:57 +0800

    Delete test_net.py

--- test_net.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-tensor = torch.tensor([0.], device='cuda:0')
-tensor4 = torch.tensor([0., 0., 0., 0.], device='cuda:0')
-if tensor4.size() == torch.Size([4]):
-    print("The tensor has size [4].")
-else:
-    print("The tensor does not have size [4].")
